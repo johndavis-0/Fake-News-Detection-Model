6a6d644b12b11aa6471e29e90d52efcb2d9de532
JohnDavis0 <133102133+johndavis-0@users.noreply.github.com>
2024-04-28 18:59:11 +0530

    BERT reconfigured. Backend updated.

--- Backend/API.py
+++ Backend/API.py
@@ -1,29 +1,26 @@
 from flask import Flask, request
+from flask_cors import cross_origin
 from keras.models import load_model
-import numpy as np
 
 app = Flask(__name__)
 
 # Load the model
-model = load_model('path_to_your_model.h5')
+model = load_model('bert_4_.keras')
 
 @app.route('/predict', methods=['POST'])
+@cross_origin()
 def predict():
     data = request.get_json(force=True)
-    prediction = model.predict([np.array([data['input']])])
+    prediction = 0
 
     # Process the prediction result as needed
     # For example, if your model returns a binary result, you might do:
 
-    if prediction == 0:
-        result = 0
-    elif prediction == 1:
-        result = 1
-    #result = int(prediction[0][0] > 0.5)
-
-    return {
-        result
-    }
+    if prediction == 0: return 0;
+        
+    elif prediction == 1: return 1;
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
+  
